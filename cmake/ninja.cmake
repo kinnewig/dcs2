2531a993fce1b0a88a7d32b6aa3a4dc6f1c3de80
Sebastian Kinnewig <sebastian@kinnewig.org>
2024-03-07 11:13:34 +0100

    rework ninja

--- cmake/ninja.cmake
+++ cmake/ninja.cmake
@@ -1,14 +1,13 @@
 include(ExternalProject)
 
-# Check if Ninja is installed
-find_program(NINJA ninja)
+set(NINJA_VERSION "1.11.1" CACHE STRING "Specify the version of Ninja to be used")
 
 # get the download url for ninja:
 file(READ ${CMAKE_CURRENT_LIST_DIR}/libraries.json json)
 string(JSON ninja_url GET ${json} ninja git)
-string(JSON ninja_tag GET ${json} ninja ${Ninja_VERSION} tag)
-if (NOT scalapack_tag)
-  message(FATAL_ERROR "Git tag for Ninja version ${Ninja_VERSION} not found in ${CMAKE_CURRENT_LIST_DIR}/libraries.json.")
+string(JSON ninja_tag GET ${json} ninja ${NINJA_VERSION} tag)
+if (NOT ninja_tag)
+  message(FATAL_ERROR "Git tag for Ninja version ${NINJA_VERSION} not found in ${CMAKE_CURRENT_LIST_DIR}/libraries.json.")
 endif()
 
 # If a custom URL for ninja is defined, use it.
@@ -21,24 +20,19 @@ if (DEFINED NINJA_CUSTOM_TAG)
   set(blis_tag ${NINJA_CUSTOM_TAG})
 endif()
 
-if(NOT NINJA)
-  set(ninja_cmake_args
-    -D CMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}/ninja/${NINJA_VERSION}
-    -D CMAKE_C_COMPILER:PATH=${CMAKE_C_COMPILER}
-    -D CMAKE_CXX_COMPILER:PATH=${CMAKE_CXX_COMPILER}
-    -D CMAKE_Fortran_COMPILER:PATH=${CMAKE_Fortran_COMPILER}
-  )
+set(ninja_cmake_args
+  -D CMAKE_INSTALL_PREFIX:PATH=${INSTALL_DIR}/ninja/${NINJA_VERSION}
+  -D CMAKE_C_COMPILER:PATH=${CMAKE_C_COMPILER}
+  -D CMAKE_CXX_COMPILER:PATH=${CMAKE_CXX_COMPILER}
+  -D CMAKE_Fortran_COMPILER:PATH=${CMAKE_Fortran_COMPILER}
+)
 
-  ExternalProject_Add(ninja
-    GIT_REPOSITORY ${NINJA_URL}
-    GIT_TAG ${NINJA_VERSION}
-    GIT_SHALLOW true
-    CMAKE_ARGS ${ninja_cmake_args}
-    CONFIGURE_HANDLED_BY_BUILD true
-    CMAKE_GENERATOR ${DEFAULT_GENERATOR}
-    INSTALL_COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_INSTALL_PREFIX}/ninja/${NINJA_VERSION}/ninja ${CMAKE_INSTALL_PREFIX}/bin/ninja
-  )
-
-  # Set the DEFAULT_GENERATOR to Ninja
-  set(DEFAULT_GENERATOR "Ninja")
-endif()
+ExternalProject_Add(ninja
+  GIT_REPOSITORY ${ninja_url}
+  GIT_TAG ${ninja_version}
+  GIT_SHALLOW true
+  CMAKE_ARGS ${ninja_cmake_args}
+  CONFIGURE_HANDLED_BY_BUILD true
+  TMP_DIR ${BUILD_DIR}
+  INSTALL_COMMAND ${CMAKE_COMMAND} -E create_symlink ${INSTALL_DIR}/ninja/${NINJA_VERSION}/ninja ${BIN_DIR}/ninja
+)
