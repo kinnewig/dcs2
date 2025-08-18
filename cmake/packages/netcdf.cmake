49eebfeed97cae4d0dd89c965c6d5179a9283c7e
Sebastian Kinnewig <sebastian@kinnewig.org>
2025-08-18 11:38:29 +0200

    Add netcdf.

---
+++ cmake/packages/netcdf.cmake
@@ -0,0 +1,27 @@
+include(ExternalProject)
+
+find_package(NETCDF)
+if(NOT NETCDF_FOUND)
+  message(STATUS "Building netcdf")
+  
+  set(netcdf_cmake_args
+    -D CMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}/netcdf/${NETCDF_VERSION}
+    -D CMAKE_C_COMPILER:PATH=${MPI_C_COMPILER}
+    -D CMAKE_BUILD_TYPE:STRING=Release
+    -D NETCDF_ENABLE_DAP:BOOL=OFF 
+    -D NETCDF_ENABLE_NETCDF_4:BOOL=ON
+    ${netcdf_cmake_args}
+  )
+
+  build_cmake_subproject("netcdf")
+
+  # Dependencies:
+  list(APPEND trilinos_dependencies "netcdf")
+endif()
+
+# add netcdf to trilinos
+list(APPEND trilinos_cmake_args "-D TPL_ENABLE_Netcdf:BOOL=ON")
+list(APPEND trilinos_cmake_args "-D Netcdf_LIBRARY_DIRS:PATH=${NETCDF_DIR}/lib")
+list(APPEND trilinos_cmake_args "-D Netcdf_INCLUDE_DIRS:PATH=${NETCDF_DIR}/include")
+# To use netcdf in Trilinos we also need to enable the follwoing packages in Trilinos
+list(APPEND trilinos_cmake_args "-D Trilinos_ENABLE_SEACAS=ON")
