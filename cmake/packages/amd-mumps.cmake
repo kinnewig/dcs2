19e9b3a9af0d07a37733c93c9a63369b9ba97ce9
Sebastian Kinnewig <sebastian@kinnewig.org>
2025-10-30 18:31:12 +0100

    Fix the linking of AMD-MUMPS into Trilinos

--- cmake/packages/amd-mumps.cmake
+++ cmake/packages/amd-mumps.cmake
@@ -53,8 +53,9 @@ list(APPEND petsc_autotool_args "--with-mumps-dir=${AMD-MUMPS_DIR}")
 list(APPEND trilinos_cmake_args "-D TPL_ENABLE_MUMPS=ON")
 list(APPEND trilinos_cmake_args "-D MUMPS_LIBRARY_DIRS:PATH=${AMD-MUMPS_DIR}/lib")
 list(APPEND trilinos_cmake_args "-D MUMPS_INCLUDE_DIRS:PATH=${AMD-MUMPS_DIR}/include")
-list(APPEND trilinos_cmake_args "-D Amesos_ENABLE_MUMPS:BOOL=ON")
-list(APPEND trilinos_cmake_args "-D Amesos2_ENABLE_MUMPS:BOOL=ON")
+#list(APPEND trilinos_cmake_args "-D Amesos_ENABLE_MUMPS:BOOL=ON")
+#list(APPEND trilinos_cmake_args "-D Amesos2_ENABLE_MUMPS:BOOL=ON")
+list(APPEND trilinos_cmake_args  "-D MUMPS_LIBRARY_NAMES:STRING=dmumps;smumps;pord")
 
 # Force deal.II to use MUMPS
 list(APPEND dealii_cmake_args "-D DEAL_II_WITH_MUMPS:BOOL=ON")
